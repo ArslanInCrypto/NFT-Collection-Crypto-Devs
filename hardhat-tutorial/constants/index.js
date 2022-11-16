d5658b17e50797543d9125cef44b5d67bb742d6e
Арслан (WL) <arslanwl@MacBook-Air-Arslan.local>
2022-11-16 21:22:37 +0500

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,9 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x8a136eF3b89106bD74E51E215f1A820599412546";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = {
+  WHITELIST_CONTRACT_ADDRESS,
+  METADATA_URL,
+};
